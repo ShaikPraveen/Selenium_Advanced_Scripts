29b190762f24ec665fe641a2decc7cc6834239ff
Manohar <info.praveen@gmail.com>
2019-02-06 20:35:24 +0530

    Praveen: I have modified some code

--- DECEMBER_SELENIUM/src/Gmail_Login.java
+++ DECEMBER_SELENIUM/src/Gmail_Login.java
@@ -19,9 +19,9 @@ public class Gmail_Login
 		
 		driver.get("https://accounts.google.com/signin/v2/sl/pwd?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		
-		driver.findElement(By.xpath("//input[@name='identifier']")).sendKeys("info.praveen77@gmail.com");
+		driver.findElement(By.xpath("//input[@name='identifier']")).sendKeys("XXKKKK77@gmail.com");
 		driver.findElement(By.xpath("//span[text()='Next']")).click();
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("praveen@1120");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("123456");
 		driver.findElement(By.xpath("//span[text()='Next']")).click();
 		
 		driver.findElement(By.xpath("//div[2]/div[3]/div/div[2]/div/a/span")).click();
